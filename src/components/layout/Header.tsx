6cd26a34246cfb7456d7732b82d53cdab2dd1932
Mheelb <maatteoo.h@gmail.com>
2025-04-08 14:36:56 +0200

    add button component and add a button in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,27 @@
 "use client";
 import { useRouter } from "next/navigation";
+import Button from "../common/Button";
 
 export default function Header() {
     const router = useRouter();
     return (
         <header>
             <div className="flex items-center space-x-15">
-            <img src="/logo.svg" alt="logo scenium" className="logo ml-15" onClick={() => router.push('/')}/>
-            <nav>
-                <ul className="flex space-x-10">
-                    <li>
-                        <a href="/scenes">Nos scènes</a>
-                    </li>
-                    <li>
-                        <a onClick={() => router.push('/about')}>À propos</a>
-                    </li>
-                </ul>
-            </nav>
+                <img src="/logo.svg" alt="logo scenium" className="logo ml-15" onClick={() => router.push('/')} />
+                <nav>
+                    <ul className="flex space-x-10">
+                        <li>
+                            <a href="/scenes">Nos scènes</a>
+                        </li>
+                        <li>
+                            <a onClick={() => router.push('/about')}>À propos</a>
+                        </li>
+                    </ul>
+                </nav>
+                <Button className="mr-30" onClick={() => router.push('/login')}>
+                    Réserver une scène
+                </Button>
             </div>
-        </header> 
+        </header>
     )
-}
+};
