66504821a85f4c06bab2c4588d2228f1e4cbd08f
klmr ✌🏼 <105491169+MaxRichet@users.noreply.github.com>
2025-05-05 11:00:55 +0200

    zoua

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -108,7 +108,7 @@ export default function Header() {
                                     }}
                                     className="text-xl"
                                 >
-                                    Contactes-nous
+                                    Contactez-nous
                                 </a>
                             </li>
                             <li>
